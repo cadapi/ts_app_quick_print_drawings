93982e5d09b08feaf2d32d0c7061572c36cec751
Jorma Hopia <jorma.hopia@hopiaengineering.fi>
2013-11-09 01:17:28 +0200

    created

---
+++ QuickPrintDrawings/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace QuickPrintDrawings
+{
+    partial class button1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.chkPDF = new System.Windows.Forms.CheckBox();
+            this.chkDWG = new System.Windows.Forms.CheckBox();
+            this.rbtn11 = new System.Windows.Forms.RadioButton();
+            this.rbtnSCALE = new System.Windows.Forms.RadioButton();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(245, 5);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(82, 28);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Write";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // chkPDF
+            // 
+            this.chkPDF.AutoSize = true;
+            this.chkPDF.Checked = true;
+            this.chkPDF.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkPDF.Location = new System.Drawing.Point(12, 12);
+            this.chkPDF.Name = "chkPDF";
+            this.chkPDF.Size = new System.Drawing.Size(47, 17);
+            this.chkPDF.TabIndex = 1;
+            this.chkPDF.Text = "PDF";
+            this.chkPDF.UseVisualStyleBackColor = true;
+            // 
+            // chkDWG
+            // 
+            this.chkDWG.AutoSize = true;
+            this.chkDWG.Checked = true;
+            this.chkDWG.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkDWG.Location = new System.Drawing.Point(65, 12);
+            this.chkDWG.Name = "chkDWG";
+            this.chkDWG.Size = new System.Drawing.Size(53, 17);
+            this.chkDWG.TabIndex = 2;
+            this.chkDWG.Text = "DWG";
+            this.chkDWG.UseVisualStyleBackColor = true;
+            // 
+            // rbtn11
+            // 
+            this.rbtn11.AutoSize = true;
+            this.rbtn11.Checked = true;
+            this.rbtn11.Location = new System.Drawing.Point(124, 12);
+            this.rbtn11.Name = "rbtn11";
+            this.rbtn11.Size = new System.Drawing.Size(40, 17);
+            this.rbtn11.TabIndex = 3;
+            this.rbtn11.TabStop = true;
+            this.rbtn11.Text = "1:1";
+            this.rbtn11.UseVisualStyleBackColor = true;
+            // 
+            // rbtnSCALE
+            // 
+            this.rbtnSCALE.AutoSize = true;
+            this.rbtnSCALE.Location = new System.Drawing.Point(170, 11);
+            this.rbtnSCALE.Name = "rbtnSCALE";
+            this.rbtnSCALE.Size = new System.Drawing.Size(73, 17);
+            this.rbtnSCALE.TabIndex = 4;
+            this.rbtnSCALE.Text = "From view";
+            this.rbtnSCALE.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 39);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(315, 23);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(12, 69);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(35, 13);
+            this.lblMessage.TabIndex = 6;
+            this.lblMessage.Text = "label1";
+            // 
+            // button1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(339, 91);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.chkDWG);
+            this.Controls.Add(this.rbtn11);
+            this.Controls.Add(this.chkPDF);
+            this.Controls.Add(this.rbtnSCALE);
+            this.Controls.Add(this.btnShow);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "button1";
+            this.Text = "Quick print drawings - (c) Hopia 2012";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.CheckBox chkPDF;
+        private System.Windows.Forms.CheckBox chkDWG;
+        private System.Windows.Forms.RadioButton rbtn11;
+        private System.Windows.Forms.RadioButton rbtnSCALE;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ QuickPrintDrawings/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Catalogs;
+using Tekla.Structures.Drawing;
+using Tekla.Structures.Model.Operations;
+using TSM = Tekla.Structures.Model;
+
+namespace QuickPrintDrawings
+{
+    public partial class button1 : Form
+    {
+        private ArrayList aDrawings = new ArrayList();
+
+        public button1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+
+            DrawingHandler MyDrawingHandler = new DrawingHandler();
+            int nDrawings = 0;
+            if (MyDrawingHandler.GetConnectionStatus())
+            {
+                DrawingEnumerator SelectedDrawings = MyDrawingHandler.GetDrawingSelector().GetSelected();
+                int iCounter = 0;
+                while (SelectedDrawings.MoveNext())
+                {
+                    iCounter++;
+                }
+                nDrawings = iCounter;
+            }
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = nDrawings;
+            progressBar1.Step = 1;
+
+            if (MyDrawingHandler.GetConnectionStatus())
+            {
+                DrawingEnumerator SelectedDrawings = MyDrawingHandler.GetDrawingSelector().GetSelected();
+                int iCounter = 1;
+                while (SelectedDrawings.MoveNext())
+                {
+                    progressBar1.PerformStep();
+
+                    Drawing currentDrawing = SelectedDrawings.Current;
+                    ListViewItem currentRow = new ListViewItem(currentDrawing.Mark);
+
+                    currentRow.SubItems.Add(currentDrawing.Name);
+                    string strHeight = currentDrawing.Layout.SheetSize.Height.ToString();
+                    string strWidth = currentDrawing.Layout.SheetSize.Width.ToString();
+                    currentRow.SubItems.Add(strWidth + "x" + strHeight);
+                    string PrinterInstance = "QP_" + strWidth + "x" + strHeight;
+
+                    //TSM.Operations.Operation.DisplayPrompt("Drawing " + currentDrawing.Name + ": " + iCounter.ToString() + "/" + nDrawings.ToString());
+                    lblMessage.Text = "";
+                    lblMessage.Text = "Drawing " + currentDrawing.Name + ": " + iCounter.ToString() + "/" + nDrawings.ToString();
+                    iCounter++;
+
+                    if (chkPDF.Checked)
+                        WriteCurrentDrawing(currentDrawing, PrinterInstance);
+
+                    if (chkDWG.Checked)
+                        PrintToFile(currentDrawing);
+                }
+            }
+
+            //TSM.Operations.Operation.DisplayPrompt("Write command completed!");
+            lblMessage.Text = "Write command completed!";
+            progressBar1.Value = 0;
+        }
+
+        private bool PrintToFile(Drawing currentDrawing)
+        {
+            bool Result = false;
+            double scale = 1.0;
+
+            if (rbtnSCALE.Checked)
+            {
+                //get scale of the first view
+                //scale = GetScaleFromTheView();
+            }
+
+            DrawingHandler MyDrawingHandler = new DrawingHandler();
+            PrintAttributes printAttributes = new PrintAttributes();
+            printAttributes.Scale = scale;
+            printAttributes.PrintToMultipleSheet = false;
+            printAttributes.NumberOfCopies = 1;
+            printAttributes.Orientation = DotPrintOrientationType.Auto;
+            printAttributes.PrintArea = DotPrintAreaType.EntireDrawing;
+            printAttributes.PrinterInstance = "DWG";
+
+            string DrawingType = GetDrawingTypeCharacter(currentDrawing);
+
+            /*
+set XS_DRAWING_PLOT_FILE_DIRECTORY=.\PlotFiles
+set XS_DRAWING_PLOT_FILE_NAME_A=%%UDA:PROJECT_USERFIELD_1%%-%%NAME.-%%%%REV_MARK?_Rev%%%%REV_MARK%%
+set XS_DRAWING_PLOT_FILE_NAME_W=%%UDA:PROJECT_USERFIELD_1%%-%%NAME.-%%%%REV_MARK?_Rev%%%%REV_MARK%%
+set XS_DRAWING_PLOT_FILE_NAME_C=%%UDA:PROJECT_USERFIELD_1%%-%%NAME.-%%%%REV_MARK?_Rev%%%%REV_MARK%%
+set XS_DRAWING_PLOT_FILE_NAME_G=%%UDA:PROJECT_USERFIELD_1%%-%%TITLE.-%%%%REV_MARK?_Rev%%%%REV_MARK%%
+set XS_DRAWING_PLOT_FILE_NAME_M=%%UDA:PROJECT_USERFIELD_1%%-%%TITLE.-%%%%REV_MARK?_Rev%%%%REV_MARK%%
+             */
+
+            //string output_path = Environment.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_DIRECTORY");
+            string output_path = "c:\\pdf";
+            string output_file = "out.dwg";
+
+            Model CurrentModel = new Model();
+            ProjectInfo ProjectInfo = CurrentModel.GetProjectInfo();
+            if (CurrentModel.GetConnectionStatus())
+            {
+                output_file = ProjectInfo.ProjectNumber;
+            }
+
+            string DrawingName = "";
+
+            //TSM.Operations.Operation.DisplayPrompt("DrawingType" + DrawingType);
+
+            switch (DrawingType)
+            {
+                case "A":
+                //output_file = Environment.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_A");
+                case "W":
+                //output_file = Environment.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_W");
+                case "C":
+                    //output_file = Environment.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_C");
+                    DrawingName = currentDrawing.Mark;
+                    DrawingName = RemoveBrackets(DrawingName);
+                    break;
+                case "G":
+                //output_file = Environment.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_G");
+                case "M":
+                    //output_file = Environment.GetEnvironmentVariable("XS_DRAWING_PLOT_FILE_NAME_M");
+                    DrawingName = currentDrawing.Name;
+                    break;
+                default:
+                    goto case "A";
+            }
+
+            DrawingName = DrawingName.Replace('.', '-');
+            output_file += "-" + DrawingName + ".dwg";
+
+            if (!String.IsNullOrEmpty(output_path) && !String.IsNullOrEmpty(output_file))
+                Result = MyDrawingHandler.PrintDrawing(currentDrawing, printAttributes, output_path + "\\" + output_file);
+
+            return Result;
+        }
+
+        private string RemoveBrackets(string inputString)
+        {
+            if (!String.IsNullOrEmpty(inputString))
+            {
+                int iS = inputString.IndexOf("[");
+                if (iS != -1)
+                    inputString = inputString.Remove(iS, 1);
+                iS = inputString.IndexOf("]");
+                if (iS != -1)
+                    inputString = inputString.Remove(iS, 1);
+            }
+
+            return inputString;
+        }
+
+        private string GetDrawingTypeCharacter(Drawing DrawingInstance)
+        {
+            string Result = "U"; // Unknown drawing
+
+            if (DrawingInstance is GADrawing)
+            {
+                Result = "G";
+            }
+            else if (DrawingInstance is AssemblyDrawing)
+            {
+                Result = "A";
+            }
+            else if (DrawingInstance is CastUnitDrawing)
+            {
+                Result = "C";
+            }
+            else if (DrawingInstance is MultiDrawing)
+            {
+                Result = "M";
+            }
+            else if (DrawingInstance is SinglePartDrawing)
+            {
+                Result = "W";
+            }
+
+            return Result;
+        }
+
+        private bool WriteCurrentDrawing(Drawing currentDrawing, string PrinterInstance)
+        {
+            bool Result = false;
+            DrawingHandler MyDrawingHandler = new DrawingHandler();
+            PrintAttributes printAttributes = new PrintAttributes();
+            printAttributes.Scale = 1.0;
+            printAttributes.PrintToMultipleSheet = false;
+            printAttributes.NumberOfCopies = 1;
+            printAttributes.Orientation = DotPrintOrientationType.Auto;
+            printAttributes.PrintArea = DotPrintAreaType.EntireDrawing;
+            printAttributes.PrinterInstance = PrinterInstance;
+
+            Result = MyDrawingHandler.PrintDrawing(currentDrawing, printAttributes);
+
+            return Result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawingHandler MyDrawingHandler = new DrawingHandler();
+
+            PrintAttributes printAttributes = new PrintAttributes();
+            printAttributes.Scale = 1.0;
+            printAttributes.PrintToMultipleSheet = false;
+            printAttributes.NumberOfCopies = 1;
+            printAttributes.Orientation = DotPrintOrientationType.Auto;
+            printAttributes.PrintArea = DotPrintAreaType.EntireDrawing;
+            printAttributes.PrinterInstance = "PDF_594x420";
+
+            if (MyDrawingHandler.GetConnectionStatus())
+            {
+                DrawingEnumerator SelectedDrawings = MyDrawingHandler.GetDrawingSelector().GetSelected();
+                while (SelectedDrawings.MoveNext())
+                {
+                    Drawing currentDrawing = SelectedDrawings.Current;
+
+                    //Get print attributes: we need only PrinterInstance
+                    MyDrawingHandler.PrintDrawing(currentDrawing, printAttributes);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
